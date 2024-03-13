650b7b1af8c44ad4d24ffc06ce0b2eeec0b67df5
userName <userEmail>
2024-03-13 10:26:52 +0800

    自定义过滤器，判断用户登录状态，完善登录功能

--- src/main/java/com/bunny/reggie/ReggieApplication.java
+++ src/main/java/com/bunny/reggie/ReggieApplication.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @MapperScan(basePackages = "com.bunny.reggie.mapper")
+@ServletComponentScan // 自动扫描webFilter
 @SpringBootApplication
 public class ReggieApplication {
 
