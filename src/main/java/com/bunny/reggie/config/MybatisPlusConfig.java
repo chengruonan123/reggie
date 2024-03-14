13617e3cfb3d809bb13dd39766e76faac6565e12
userName <userEmail>
2024-03-14 15:36:20 +0800

    新增员工信息分页查询功能

--- src/main/java/com/bunny/reggie/config/MybatisPlusConfig.java
+++ src/main/java/com/bunny/reggie/config/MybatisPlusConfig.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 /**
  * 配置MP的分页插件
  */
