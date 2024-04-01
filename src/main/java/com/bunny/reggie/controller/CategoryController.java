46d11c920caffa26c5ba3172c52e4c818e3aebc0
userName <userEmail>
2024-04-01 13:05:06 +0800

    修改分类信息

--- src/main/java/com/bunny/reggie/controller/CategoryController.java
+++ src/main/java/com/bunny/reggie/controller/CategoryController.java
@@ -18,48 +18,64 @@ public class CategoryController {
 
     /**
      * 新增分类
+     *
      * @param category
      * @return
      */
     @PostMapping
-    public R<String> save(@RequestBody Category category){
-        log.info("category:{}",category);
+    public R<String> save(@RequestBody Category category) {
+        log.info("category:{}", category);
         categoryService.save(category);
         return R.success("新增分类成功！");
     }
 
     /**
      * 分页信息查询
+     *
      * @param page
      * @param pageSize
      * @return
      */
     @GetMapping("/page")
-    public R<Page> page(int page, int pageSize){
+    public R<Page> page(int page, int pageSize) {
         //分页构造器
-        Page<Category> pageInfo=new Page<>(page,pageSize);
+        Page<Category> pageInfo = new Page<>(page, pageSize);
         //条件构造器
-        LambdaQueryWrapper<Category> queryWrapper=new LambdaQueryWrapper<>();
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
         //添加排序条件，根据sort进行排序
         queryWrapper.orderByAsc(Category::getSort);
 
         //进行分页查询
-        categoryService.page(pageInfo,queryWrapper);
+        categoryService.page(pageInfo, queryWrapper);
 
         return R.success(pageInfo);
     }
 
     /**
      * 根据id删除分类
+     *
      * @param id
      * @return
      */
     @DeleteMapping
-    public R<String> delete(Long id){
-        log.info("删除分类：ID为：{}",id);
+    public R<String> delete(Long id) {
+        log.info("删除分类：ID为：{}", id);
 
         //categoryService.removeById(id);
         categoryService.remove(id);
         return R.success("分类信息删除成功！");
+    }
+
+    /**
+     * 根据id修改分类信息
+     * @param category
+     * @return
+     */
+
+    @PutMapping
+    public R<String> update(@RequestBody Category category) {
+        log.info("修改分类信息：{}", category);
+        categoryService.updateById(category);
+        return R.success("修改分类成功！");
     }
 }
