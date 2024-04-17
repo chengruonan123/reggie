9a86cb73cbea770f64655ea216f8fbaa4c626f3e
userName <userEmail>
2024-04-17 09:55:18 +0800

    菜品信息分页查询

--- src/main/java/com/bunny/reggie/controller/CategoryController.java
+++ src/main/java/com/bunny/reggie/controller/CategoryController.java
@@ -3,13 +3,18 @@ package com.bunny.reggie.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bunny.reggie.common.R;
+import com.bunny.reggie.dto.DishDto;
 import com.bunny.reggie.entity.Category;
+import com.bunny.reggie.entity.Dish;
 import com.bunny.reggie.service.CategoryService;
+import com.bunny.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -17,6 +22,7 @@ import java.util.List;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
 
     /**
      * 新增分类
@@ -70,6 +76,7 @@ public class CategoryController {
 
     /**
      * 根据id修改分类信息
+     *
      * @param category
      * @return
      */
@@ -83,19 +90,21 @@ public class CategoryController {
 
     /**
      * 根据条件查询分类数据
+     *
      * @param category
      * @return
      */
     @GetMapping("/list")
-    public R<List<Category>> list(Category category){
+    public R<List<Category>> list(Category category) {
         //条件构造器
-        LambdaQueryWrapper<Category> queryWrapper=new LambdaQueryWrapper<>();
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
         //添加条件
-        queryWrapper.eq(category.getType()!=null,Category::getType,category.getType());
+        queryWrapper.eq(category.getType() != null, Category::getType, category.getType());
         //添加排序条件
         queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
 
-        List<Category> list=categoryService.list(queryWrapper);
+        List<Category> list = categoryService.list(queryWrapper);
         return R.success(list);
     }
+
 }
